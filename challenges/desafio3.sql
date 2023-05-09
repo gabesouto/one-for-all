65855200d7118973d4a040f47c491740d22946b5
gabesouto <soutogabriel04@gmail.com>
2023-05-09 16:59:50 -0300

    Desafio 3

--- challenges/desafio3.sql
+++ challenges/desafio3.sql
@@ -1,12 +1,11 @@
-USE SpotifyClone;
 
 SELECT 
 	u.user_name AS pessoa_usuaria,
 	COUNT(h.song_id) AS musicas_ouvidas,
 	ROUND(SUM(c.song_duration )/60, 2) AS total_minutos 
-    FROM user as u
-    INNER JOIN history as h
+    FROM SpotifyClone.user as u
+    INNER JOIN SpotifyClone.history as h
     ON u.user_id = h.user_id
-	INNER JOIN  song AS c ON h.song_id = c.song_id
+	INNER JOIN  SpotifyClone.song AS c ON h.song_id = c.song_id
     GROUP BY u.user_name 
     ORDER BY u.user_name; 
